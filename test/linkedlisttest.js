7fa29b6434ded9c431b2206a39c7da31bd2be511
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-06 21:05:56 -0700

    tests all written

--- test/linkedlisttest.js
+++ test/linkedlisttest.js
@@ -1,33 +1,35 @@
 var expect = require('chai').expect;
-var Movie = require('../lib/linkedlist.js');
+var LinkedList = require('../lib/linkedlist.js');
 
 describe('LinkedList', function() {
+
   it('should be a constructor', function() {
-    var linked = new LinkedList;
+    var linked = new LinkedList();
     expect(linked).to.have.property('head').that.equals(null);
   }),
 
   it('should have an insert method', function() {
-    // given that I instantiate a new linkedlist
-    // if I call the insert method
-    // the head value should be a new node with the new value
+    var linked = new LinkedList();
+    linked.insert('hello world');
+    expect(linked).to.have.property('head').that.has.property('value').that.equals('hello world');
   }),
 
   it('should have a search method', function() {
-    // Given that I instantiate a new linkedlist
-    // when I search for a given value
-    // then if that value exists, that node should be returned
+    var linked = new LinkedList();
+    linked.insert('hello world');
+    expect(linked).search('hello world')['value'].to.equal('hello world');
   }),
 
   it('search should return null if nothing is found', function() {
-    // Given that I have a linked list
-    // when I search for a value that is not in the list
-    // Then the method should return null
+    var linked = new LinkedList();
+    expect(linked).search('howdy').to.equal(null);
   }),
 
   it('should have a remove method', function() {
-    // Given when I have a linked list
-    // when i call remove for a given value
-    // then that node should be removed from the linked list
+    var linked = new LinkedList();
+    linked.insert('hello world');
+    expect(linked).search('hello world')['value'].to.equal('hello world');
+    linked.remove('hello world');
+    expect(linked).search('hello world').to.equal(null);
   });
 });
